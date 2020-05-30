0934260b1e18353a302b706963937875536351f2
Greg Fodor <gfodor@mozilla.com>
2020-05-30 01:59:45 +0000

    Add CORS

--- index.js
+++ index.js
@@ -321,7 +321,8 @@ async function createExpressApp()
 				ccu += room.getCCU();
 			}
 
-			res.status(200).json({ ccu });
+			// TODO remove CORS headers once swithced over
+			res.header("Access-Control-Allow-Origin", ["*"]).status(200).json({ ccu });
 		});
 
 	/**
