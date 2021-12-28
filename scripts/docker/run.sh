0acf197abcc6ec913beae6b02e9885327c1dfa65
Geng Tan <geng.tan@limeade.com>
2021-12-28 12:02:17 -0500

    more run.sh fix ...

--- scripts/docker/run.sh
+++ scripts/docker/run.sh
@@ -1,4 +1,6 @@
 # TODO: need a better one
+PUB_IP_CURL=https://ipinfo.io/ip
+
 healthcheck(){
     while true; do (echo -e 'HTTP/1.1 200 OK\r\n\r\n 1') | nc -lp 1111 > /dev/null; done
 }
@@ -7,7 +9,7 @@ healthcheck(){
 healthcheck &
 echo -e $(echo -e ${perms_key//\n/n}) > /app/certs/perms.pub.pem        
 head -3 /app/certs/perms.pub.pem
-export MEDIASOUP_ANNOUNCED_IP=$(curl ${{ env.PUB_IP_CURL }})
+export MEDIASOUP_ANNOUNCED_IP=$(curl ${PUB_IP_CURL})
 echo "MEDIASOUP_ANNOUNCED_IP: $MEDIASOUP_ANNOUNCED_IP"
 export INTERACTIVE=nope
 
