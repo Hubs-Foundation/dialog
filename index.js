22668430d8db4a64153fcfe39e2bc89307285074
Geng Tan <geng.tan@limeade.com>
2022-05-12 01:06:03 -0400

    wip

--- index.js
+++ index.js
@@ -30,7 +30,6 @@ const util = require('util');
 const readFile = util.promisify(fs.readFile);
 
 var utils = require('./lib/utils')
-var workerLoadMap = utils.workerLoadMap
 
 const logger = new Logger();
 const queue = new AwaitQueue();
@@ -104,7 +103,7 @@ async function runMediasoupWorkers()
 		});
 
 		mediasoupWorkers.push(worker);
-		workerLoadMap.set(worker._pid, 0)
+		utils.workerLoadMap_set(worker._pid, 0)
 
 		setInterval(async () =>
 		{
