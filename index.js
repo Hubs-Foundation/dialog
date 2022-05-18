5f23d94b0092771f41e7ad438ccde6b2e19bb97a
Geng Tan <geng.tan@limeade.com>
2022-05-18 11:18:58 -0400

    wip

--- index.js
+++ index.js
@@ -331,7 +331,8 @@ async function createExpressApp()
 			}
 
 			const workerLoadMap = utils.workerLoadMap_get()
-			workerLoadMap.set("total_ccu", ccu)
+			workerLoadMap.set("_total_ccu", ccu)
+			workerLoadMap.set("_hostname", os.hostname())
 
 			// TODO remove CORS header once live
 			// res.header("Access-Control-Allow-Origin", ["*"]).status(200).json({ ccu });
