9f518fe262cc74c9f2c1776f88c126a604165397
Brian Peiris <brianpeiris@gmail.com>
2022-08-18 12:56:29 -0400

    Remove broadcaster endpoints

--- index.js
+++ index.js
@@ -103,10 +103,10 @@ async function runMediasoupWorkers()
 		});
 
 		mediasoupWorkers.push(worker);
-		utils.workerLoadMan.set(worker._pid, { peerCnt: 0, roomReqCnt: 0, rooms: new Map() })
+		utils.workerLoadMan.set(worker._pid, { peerCnt: 0, roomReqCnt: 0, rooms: new Map() });
 	}
 
-	utils.workerLoadMan.runSurvey()
+	utils.workerLoadMan.runSurvey();
 
 	setInterval(async () => { 
 		const startTimestampNs = process.hrtime.bigint();
@@ -139,181 +139,6 @@ async function createExpressApp()
 			next();
 		});
 
-	/**
-	 * API GET resource that returns the mediasoup Router RTP capabilities of
-	 * the room.
-	 */
-	expressApp.get(
-		'/rooms/:roomId', (req, res) =>
-		{
-			const data = req.room.getRouterRtpCapabilities();
-
-			res.status(200).json(data);
-		});
-
-	/**
-	 * POST API to create a Broadcaster.
-	 */
-	expressApp.post(
-		'/rooms/:roomId/broadcasters', async (req, res, next) =>
-		{
-			const {
-				id,
-				displayName,
-				device,
-				rtpCapabilities
-			} = req.body;
-
-			try
-			{
-				const data = await req.room.createBroadcaster(
-					{
-						id,
-						displayName,
-						device,
-						rtpCapabilities
-					});
-
-				res.status(200).json(data);
-			}
-			catch (error)
-			{
-				next(error);
-			}
-		});
-
-	/**
-	 * DELETE API to delete a Broadcaster.
-	 */
-	expressApp.delete(
-		'/rooms/:roomId/broadcasters/:broadcasterId', (req, res) =>
-		{
-			const { broadcasterId } = req.params;
-
-			req.room.deleteBroadcaster({ broadcasterId });
-
-			res.status(200).send('broadcaster deleted');
-		});
-
-	/**
-	 * POST API to create a mediasoup Transport associated to a Broadcaster.
-	 * It can be a PlainTransport or a WebRtcTransport depending on the
-	 * type parameters in the body. There are also additional parameters for
-	 * PlainTransport.
-	 */
-	expressApp.post(
-		'/rooms/:roomId/broadcasters/:broadcasterId/transports',
-		async (req, res, next) =>
-		{
-			const { broadcasterId } = req.params;
-			const { type, rtcpMux, comedia } = req.body;
-
-			try
-			{
-				const data = await req.room.createBroadcasterTransport(
-					{
-						broadcasterId,
-						type,
-						rtcpMux,
-						comedia
-					});
-
-				res.status(200).json(data);
-			}
-			catch (error)
-			{
-				next(error);
-			}
-		});
-
-	/**
-	 * POST API to connect a Transport belonging to a Broadcaster. Not needed
-	 * for PlainTransport if it was created with comedia option set to true.
-	 */
-	expressApp.post(
-		'/rooms/:roomId/broadcasters/:broadcasterId/transports/:transportId/connect',
-		async (req, res, next) =>
-		{
-			const { broadcasterId, transportId } = req.params;
-			const { dtlsParameters } = req.body;
-
-			try
-			{
-				const data = await req.room.connectBroadcasterTransport(
-					{
-						broadcasterId,
-						transportId,
-						dtlsParameters
-					});
-
-				res.status(200).json(data);
-			}
-			catch (error)
-			{
-				next(error);
-			}
-		});
-
-	/**
-	 * POST API to create a mediasoup Producer associated to a Broadcaster.
-	 * The exact Transport in which the Producer must be created is signaled in
-	 * the URL path. Body parameters include kind and rtpParameters of the
-	 * Producer.
-	 */
-	expressApp.post(
-		'/rooms/:roomId/broadcasters/:broadcasterId/transports/:transportId/producers',
-		async (req, res, next) =>
-		{
-			const { broadcasterId, transportId } = req.params;
-			const { kind, rtpParameters } = req.body;
-
-			try
-			{
-				const data = await req.room.createBroadcasterProducer(
-					{
-						broadcasterId,
-						transportId,
-						kind,
-						rtpParameters
-					});
-
-				res.status(200).json(data);
-			}
-			catch (error)
-			{
-				next(error);
-			}
-		});
-
-	/**
-	 * POST API to create a mediasoup Consumer associated to a Broadcaster.
-	 * The exact Transport in which the Consumer must be created is signaled in
-	 * the URL path. Query parameters must include the desired producerId to
-	 * consume.
-	 */
-	expressApp.post(
-		'/rooms/:roomId/broadcasters/:broadcasterId/transports/:transportId/consume',
-		async (req, res, next) =>
-		{
-			const { broadcasterId, transportId } = req.params;
-			const { producerId } = req.query;
-
-			try
-			{
-				const data = await req.room.createBroadcasterConsumer(
-					{
-						broadcasterId,
-						transportId,
-						producerId
-					});
-
-				res.status(200).json(data);
-			}
-			catch (error)
-			{
-				next(error);
-			}
-		});
 	/**
 	 * Error handler.
 	 */
@@ -378,9 +203,9 @@ async function createAdminExpressApp()
 	expressAdminApp.get(
 		'/report', (req, res) =>
 		{
-			const report = new Map(utils.workerLoadMan.get())
-			report.set('_hostname', os.hostname())
-			report.set('_capacity', utils.workerLoadMan.sum())
+			const report = new Map(utils.workerLoadMan.get());
+			report.set('_hostname', os.hostname());
+			report.set('_capacity', utils.workerLoadMan.sum());
 			res.set({ 'Content-Type': 'application/json' })
 				.status(200)
 				.send(JSON.stringify(report, utils.stableSortReplacer, 2));
