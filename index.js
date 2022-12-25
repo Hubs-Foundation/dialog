462f39be87d6b84fd8217598eab202d94d2ac449
Geng Tan <tanfarming@hotmail.com>
2022-12-25 11:02:11 -0500

    words

--- index.js
+++ index.js
@@ -220,7 +220,7 @@ async function createAdminExpressApp()
 				console.log(report)
 				res.set({ 'Content-Type': 'application/json' })
 					.status(200)
-					.send(report.toString());
+					.send(JSON.stringify(report));
 			});
 			/**
 			 * dump peer
@@ -240,7 +240,7 @@ async function createAdminExpressApp()
 					console.log(report)
 					res.set({ 'Content-Type': 'application/json' })
 						.status(200)
-						.send(report.toString());
+						.send(JSON.stringify(report));
 				});
 
 	/**
